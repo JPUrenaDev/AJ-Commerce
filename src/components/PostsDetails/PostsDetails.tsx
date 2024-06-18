00fc3696580f05b9cf50ecdb34c133ba2fae2601
JPUrenaDev <jeanpaul_34_1@Hotmail.com>
2024-06-18 19:22:16 -0400

    adding more features to the details product such change photo when you click back and foward arrow

--- src/components/PostsDetails/PostsDetails.tsx
+++ src/components/PostsDetails/PostsDetails.tsx
@@ -7,43 +7,103 @@ import { DoOffer } from "./ui/DoOffer";
 import { FaLocationDot } from "react-icons/fa6";
 import { FaTag } from "react-icons/fa";
 import { SellerCard } from "./SellerCard";
-
+import { RiErrorWarningFill } from "react-icons/ri";
+import { useState } from "react";
 export const PostsDetails = () => {
+  const [photosDetailPosts, setPhotosDetailPosts] = useState([
+    {
+      img: "https://www.corotos.com.do/imagenes/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBCUFRSaGdJPSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--c5a5d867cc35f61a30d89e6855152f68ea9956f0/9e2ef873_0c2c_42e6_a2f0_55398776c154_jpg?variant_options%5Bformat%5D=webp&variant_options%5Bresize_to_fit%5D%5B%5D=1280&variant_options%5Bresize_to_fit%5D%5B%5D=720",
+    },
+    {
+      img: "https://www.corotos.com.do/imagenes/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBCUFRSaGdJPSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--c5a5d867cc35f61a30d89e6855152f68ea9956f0/9e2ef873_0c2c_42e6_a2f0_55398776c154_jpg?variant_options%5Bformat%5D=webp&variant_options%5Bresize_to_fit%5D%5B%5D=1280&variant_options%5Bresize_to_fit%5D%5B%5D=720",
+    },
+    {
+      img: "https://www.corotos.com.do/imagenes/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBCUExSaGdJPSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--899aa1bd92789a9d98b9d42cb8978310ef694589/aeb6d040_f977_4e2e_a512_e20644a8d6d1_jpg?variant_options%5Bformat%5D=webp&variant_options%5Bresize_to_fit%5D%5B%5D=1280&variant_options%5Bresize_to_fit%5D%5B%5D=720",
+    },
+    {
+      img: "https://www.corotos.com.do/imagenes/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBCUFBSaGdJPSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--6b08eb7df4388b8d00fce1de058c044cd6afb051/351b0a1b_e994_43f1_9746_89b59bee0223_jpg?variant_options%5Bformat%5D=webp&variant_options%5Bresize_to_fit%5D%5B%5D=1280&variant_options%5Bresize_to_fit%5D%5B%5D=720",
+    },
+  ]);
+
+  const [currentPhoto, setCurrentPhoto] = useState(0);
+
+  const onNextPhoto = () => {
+    console.log(currentPhoto, photosDetailPosts.length);
+    if (currentPhoto == photosDetailPosts.length - 1) {
+      setCurrentPhoto(0);
+      return;
+    }
+    setCurrentPhoto((previousValue) => previousValue + 1);
+  };
+
+  const onPreviousPhoto = () => {
+    //IF THE CURRENT PHOTO = 0, I WANT TO SHOW THE LAST PHOTO
+    if (currentPhoto == 0) {
+      setCurrentPhoto(photosDetailPosts.length - 1);
+      return;
+    }
+    setCurrentPhoto((previousValue) => previousValue - 1);
+  };
   return (
-    <div className="flex items-start mt-4 w-[1300px] mx-auto gap-[100px] pb-[30px]">
-      <div className="flex items-center ">
-        <div className="rounded-r-lg bg-black w-[35px]  py-[20px] mr-4 ml-4 opacity-75 ml-1 cursor-pointer">
-          <IoIosArrowBack size={30} color="white" className="" />
+    <div className="flex items-start mt-4 w-[1300px]  mx-auto gap-[30px] pb-[30px]">
+      <div className="flex items-center  px-[60px] ">
+        <div className="rounded-r-lg bg-black w-[35px]  py-[20px] mr-4 ml-4 mb-[300px] opacity-75 ml-1 cursor-pointer">
+          <IoIosArrowBack
+            size={30}
+            color="white"
+            className=""
+            onClick={onPreviousPhoto}
+          />
         </div>
         <div>
           <div className="flex flex-col">
             <img
-              className="object-cover w-[400px] h-[400px] px-9 cursor-zoom-in"
-              src="https://www.corotos.com.do/imagenes/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBCUFRSaGdJPSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--c5a5d867cc35f61a30d89e6855152f68ea9956f0/9e2ef873_0c2c_42e6_a2f0_55398776c154_jpg?variant_options%5Bformat%5D=webp&variant_options%5Bresize_to_fit%5D%5B%5D=1280&variant_options%5Bresize_to_fit%5D%5B%5D=720"
+              className="object-cover w-[500px]  mx-auto h-[400px] px-9 cursor-zoom-in"
+              src={`${photosDetailPosts[currentPhoto].img}`}
             ></img>
             <div className="flex gap-3">
-              <img
-                className=" object-cover w-[60px] opacity-50 top-[140px] h-[50px] mb-4 mt-[10px] cursor-zoom-in"
-                src="https://www.corotos.com.do/imagenes/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBCUFRSaGdJPSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--c5a5d867cc35f61a30d89e6855152f68ea9956f0/9e2ef873_0c2c_42e6_a2f0_55398776c154_jpg?variant_options%5Bformat%5D=webp&variant_options%5Bresize_to_fit%5D%5B%5D=1280&variant_options%5Bresize_to_fit%5D%5B%5D=720"
-              ></img>
-              <img
-                className=" object-cover w-[60px] opacity-50 top-[140px] h-[50px] mb-4 mt-[10px] cursor-zoom-in"
-                src="https://www.corotos.com.do/imagenes/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBCUFRSaGdJPSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--c5a5d867cc35f61a30d89e6855152f68ea9956f0/9e2ef873_0c2c_42e6_a2f0_55398776c154_jpg?variant_options%5Bformat%5D=webp&variant_options%5Bresize_to_fit%5D%5B%5D=1280&variant_options%5Bresize_to_fit%5D%5B%5D=720"
-              ></img>
-              <img
-                className=" object-cover w-[60px] opacity-50 top-[140px] h-[50px] mb-4 mt-[10px] cursor-zoom-in"
-                src="https://www.corotos.com.do/imagenes/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBCUExSaGdJPSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--899aa1bd92789a9d98b9d42cb8978310ef694589/aeb6d040_f977_4e2e_a512_e20644a8d6d1_jpg?variant_options%5Bformat%5D=webp&variant_options%5Bresize_to_fit%5D%5B%5D=1280&variant_options%5Bresize_to_fit%5D%5B%5D=720"
-              ></img>
-              <img
-                className=" object-cover w-[60px] opacity-50 top-[140px] h-[50px] mb-4 mt-[10px] cursor-zoom-in"
-                src="https://www.corotos.com.do/imagenes/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBCUFBSaGdJPSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--6b08eb7df4388b8d00fce1de058c044cd6afb051/351b0a1b_e994_43f1_9746_89b59bee0223_jpg?variant_options%5Bformat%5D=webp&variant_options%5Bresize_to_fit%5D%5B%5D=1280&variant_options%5Bresize_to_fit%5D%5B%5D=720"
-              ></img>
+              {/** RENDER MINI PHOTOS  */}
+              {photosDetailPosts.map((photos, index) => (
+                <>
+                  <img
+                    className={`object-cover w-[60px] ${
+                      index == currentPhoto ? "opacity-100" : "opacity-50"
+                    } top-[140px] h-[50px] mb-4 mt-[10px] cursor-zoom-in`}
+                    src={photos.img}
+                  ></img>
+                </>
+              ))}
             </div>
+          </div>
+          <div className="flex mt-5 items-center gap-[280px] text-gray-500">
+            <h2>Condicion</h2>
+            <div className="flex items-center gap-2">
+              <h2>Usado</h2>
+              <RiErrorWarningFill
+                color="gray"
+                size={20}
+                className="cursor-pointer"
+              />
+            </div>
+          </div>
+          <hr className="border-salate-500 w-full mt-5"></hr>
+          <div className="flex mt-5 items-center gap-[230px] text-gray-500">
+            <h2>Entrega a domicilio</h2>
+            <h2>No</h2>
+          </div>
+          <hr className="border-salate-500 w-full mt-5 mb-6"></hr>
+          <div>
+            <h2 className="font-semibold text-[20px] mb-4">Descripción</h2>
+            <p>
+              Traje Militar - verde olivo - chaqueta pantalón y falda . Buen
+              estado solos dos veces usado Con accesorios Excelentes condiciones
+              Para talla mujer mediana petite
+            </p>
           </div>
         </div>
 
-        <div className="rounded-l-lg  bg-black w-[35px]  py-[20px] mr-4 ml-4 opacity-75 pl-1 cursor-pointer">
-          <IoIosArrowForward size={30} color="white" />
+        <div className="rounded-l-lg  bg-black w-[35px] mb-[90px] py-[20px] mb-[300px] mr-4 ml-4 opacity-75 pl-1 cursor-pointer">
+          <IoIosArrowForward size={30} color="white" onClick={onNextPhoto} />
         </div>
       </div>
 
@@ -62,9 +122,9 @@ export const PostsDetails = () => {
           <div className="mt-5 ">
             <hr className="border-salate-500"></hr>
 
-            <h2 className="py-6">Inventario disponible</h2>
+            <h2 className="py-6  text-[20px]">Inventario disponible</h2>
             <h2>
-              Cantidad:<span className="bg-bold">1 unidad</span>
+              Cantidad:<span className="font-bold"> 1 unidad</span>
               <hr className="border-salate-500 mt-5"></hr>
             </h2>
             <div className="mt-4 flex flex-col gap-8">
--- src/components/PostsDetails/SellerChat.tsx
+++ src/components/PostsDetails/SellerChat.tsx
@@ -3,23 +3,28 @@ import { ButtonModel } from "../ui/ButtonModel";
 
 export const SellerChat = () => {
   return (
-    <div className=" bg-sky-50 h-[99px] border border-indigo-300 px-3 py-2   rounded-lg ">
-      <h3 className="font-medium text-[13px] mb-2">Chatear con el vendedor</h3>
-      <div className="flex items-center gap-3 ">
-        <input
-          placeholder="Hola,Esta disponible?"
-          className="border  w-[250px] h-[40px] rounded-lg px-2"
-        ></input>
+    <div className=" bg-sky-50 h-[99px] border border-indigo-300  py-2   rounded-lg ">
+      <div className="px-3">
+        <h3 className="font-medium text-[13px] mb-2">
+          Chatear con el vendedor
+        </h3>
+        <div className="flex items-center gap-3 ">
+          <input
+            placeholder="Hola,Esta disponible?"
+            className="border  w-[250px] h-[40px] rounded-lg px-2"
+          ></input>
 
-        <ButtonModel
-          width={"80px"}
-          height={"30px"}
-          bgColor={"#1E90FF"}
-          textColor={"white"}
-        >
-          Enviar
-        </ButtonModel>
+          <ButtonModel
+            width={"80px"}
+            height={"30px"}
+            bgColor={"#1E90FF"}
+            textColor={"white"}
+          >
+            Enviar
+          </ButtonModel>
+        </div>
       </div>
+
       <div className="mt-10">
         <ButtonModel
           width={"100%"}
