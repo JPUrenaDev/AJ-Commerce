f5a869e4caa487b5dc4e304548c2b9de24f3a610
JPUrenaDev <jeanpaul_34_1@Hotmail.com>
2024-07-01 22:15:19 -0400

    add the state page

--- src/pages/Estate.tsx
+++ src/pages/Estate.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BsCurrencyDollar } from "react-icons/bs";
 export const Estate = () => {
   return (
-    <div className="flex-grow w-full h-full  mt-[-100px] ml-[200px]  ">
+    <div className="flex-grow w-full h-full  mt-[-100px] ml-[200px]  flex items-center justify-between ">
       <div className="w-1/3  flex flex-col items-center font-bold text-blue-900">
         <h2 className="text-black">INMUEBLES</h2>
         <h1 className="text-[30px]">
@@ -10,7 +10,7 @@ export const Estate = () => {
         </h1>
         <h1 className="text-[30px]">Sueños en Corotos</h1>
 
-        <div className="bg-sky-100 w-full h-[130px] rounded-md">
+        <div className="bg-sky-200 w-full h-[130px] rounded-md">
           <div className="flex items-center justify-center gap-3 pt-1">
             <h3>Todos</h3>
             <h3>Venta</h3>
@@ -31,6 +31,14 @@ export const Estate = () => {
             ></input>
           </div>
         </div>
+        <div className="w-full mt-3 ">
+          <button className="bg-yellow-400 w-full rounded-full h-[40px] shadow-md">
+            Realizar búsqueda
+          </button>
+        </div>
+      </div>
+      <div>
+        <img src="https://www.corotos.com.do/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBCRTZIaXdJPSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--c3f70995b50e931f381fb5dc451cfeb1fbe97acf/ilustracion%20inmueble%202.png"></img>
       </div>
     </div>
   );
